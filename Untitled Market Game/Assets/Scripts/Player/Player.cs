3aba801573ad21975a1180ae6faee4d732680428
Simon <666ohyeah999@gmail.com>
2019-10-05 13:22:16 +0200

    Basic move

---
+++ Untitled Market Game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Camera cam;
+
+    private CharacterController _controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = (Input.GetAxis("Horizontal") * new Vector3(cam.transform.right.x, 0, cam.transform.right.z) + Input.GetAxis("Vertical") * new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z)).normalized;
+        _controller.Move(move * Time.deltaTime * speed);
+    }
+}
