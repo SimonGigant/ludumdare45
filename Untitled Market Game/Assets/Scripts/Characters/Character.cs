c7f7865725ef4704223a68a3c571773cb72db3d5
Simon <666ohyeah999@gmail.com>
2019-10-05 18:50:07 +0200

    Object format + improve dialog

--- Untitled Market Game/Assets/Scripts/Characters/Character.cs
+++ Untitled Market Game/Assets/Scripts/Characters/Character.cs
@@ -2,29 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Texts
+{
+    public List<string> list;
+}
+
+[System.Serializable]
+public class BigTexts
+{
+    public List<Texts> list;
+    public List<List<string>> Lists()
+    {
+        List<List<string>> res = new List<List<string>>();
+        foreach(Texts i in list)
+        {
+            res.Add(i.list);
+        }
+        return res;
+    }
+}
+
 public class Character : MonoBehaviour
 {
-    public List<string> firstMeetDialogue = new List<string>();
-    public List<string> dialogue = new List<string>();
+    public BigTexts dialogInInspector = new BigTexts();
+    private List<List<string>> dialogue;
     private bool met;
 
     public GameObject bubble;
+
+    public float heightBubble;
+    
     private GameObject activeBubble = null;
 
     private void Start()
     {
         met = false;
+        dialogue = dialogInInspector.Lists();
     }
 
     public void MeetCharacter()
     {
         if (met)
         {
-            Talk(dialogue);
+            Talk(dialogue[1]);
         }
         else
         {
-            Talk(firstMeetDialogue);
+            Talk(dialogue[0]);
             met = true;
         }
     }
@@ -35,7 +60,7 @@ public class Character : MonoBehaviour
         {
             Quaternion quat = transform.rotation;
             quat.y += -90;
-            activeBubble = GameObject.Instantiate(bubble,transform.position + new Vector3(0,6,0), quat);
+            activeBubble = GameObject.Instantiate(bubble,transform.position + new Vector3(0, heightBubble, 0), quat);
             activeBubble.transform.parent = gameObject.transform;
             Bubble b = activeBubble.GetComponent<Bubble>();
             b.GiveDialogue(text);
--- Untitled Market Game/Assets/Scripts/Objects/Object.cs
+++ Untitled Market Game/Assets/Scripts/Objects/Object.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum ObjectType{Touillette = 2, Cle = 3, Balle = 4, Revue = 5, Scie = 6, Rein = 7, Thune = 8, Ticket = 9};
 public class Object : MonoBehaviour
 {
-    
+    public ObjectType type;
 }
